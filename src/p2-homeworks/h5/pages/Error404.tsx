97dde01c605d1e3a7e761dec952909d6b50bb4cc
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-18 15:29:51 +0300

    d

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import s from './Error404.module.css'
+import { NavLink } from "react-router-dom";
+
 
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.error}>
+            <div className={s.error__number} >404</div>
+            <div className={s.error__message} >Page not found!</div>
+            <div className={s.error__symbol} >—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <div className={s.error__redirect}><NavLink to={`/`}>{"<= Вернуться на главную страницу"}</NavLink></div>
         </div>
     );
 }
