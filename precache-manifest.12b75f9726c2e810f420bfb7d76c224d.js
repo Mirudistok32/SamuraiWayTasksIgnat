286c5a85fc118f32ea82b4c270f2b4dbeb1d721d
Mirudistok <mirudistok32@yandex.ru>
2020-08-18 10:44:30 +0300

    Updates

---
+++ precache-manifest.12b75f9726c2e810f420bfb7d76c224d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c02ee2e6e5b0da8762edeacd3bec66",
+    "url": "/SamuraiWayTasksIgnat/index.html"
+  },
+  {
+    "revision": "abb011ab7a8b5dde5c42",
+    "url": "/SamuraiWayTasksIgnat/static/css/main.6dd61726.chunk.css"
+  },
+  {
+    "revision": "e182ae3815caa8365201",
+    "url": "/SamuraiWayTasksIgnat/static/js/2.82b56cfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SamuraiWayTasksIgnat/static/js/2.82b56cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb011ab7a8b5dde5c42",
+    "url": "/SamuraiWayTasksIgnat/static/js/main.d5b4ca73.chunk.js"
+  },
+  {
+    "revision": "111ee37fe2d1508f7bf8",
+    "url": "/SamuraiWayTasksIgnat/static/js/runtime-main.be6199cc.js"
+  }
+]);
