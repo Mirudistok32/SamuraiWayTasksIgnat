5b7655919033b10ae00a90e8b94a1926ca84d6fc
Mirudistok32 <mirudistok32@yandex.ru>
2020-08-23 15:49:00 +0300

    Updates

---
+++ precache-manifest.74eabe58e7325de9c00f02498641fcfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b952b6fb9a8735b96084fa63b5aedf2",
+    "url": "/SamuraiWayTasksIgnat/index.html"
+  },
+  {
+    "revision": "b261bd11816c60c04792",
+    "url": "/SamuraiWayTasksIgnat/static/css/main.7bac3f3b.chunk.css"
+  },
+  {
+    "revision": "42e87feed8bdcd529d95",
+    "url": "/SamuraiWayTasksIgnat/static/js/2.66d4a781.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SamuraiWayTasksIgnat/static/js/2.66d4a781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b261bd11816c60c04792",
+    "url": "/SamuraiWayTasksIgnat/static/js/main.da00b2ca.chunk.js"
+  },
+  {
+    "revision": "111ee37fe2d1508f7bf8",
+    "url": "/SamuraiWayTasksIgnat/static/js/runtime-main.be6199cc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SamuraiWayTasksIgnat/precache-manifest.2fdf4f7745ca48bbd13076aef7d4ec22.js"
+  "/SamuraiWayTasksIgnat/precache-manifest.74eabe58e7325de9c00f02498641fcfb.js"
 );
 
 self.addEventListener('message', (event) => {
