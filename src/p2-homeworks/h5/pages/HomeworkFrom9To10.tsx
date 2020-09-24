8aa614d5b1c2073940d5b07e0c131c518935ab3e
Mirudistok32 <mirudistok32@yandex.ru>
2020-09-24 22:14:34 +0300

    Add redux and react-redux

--- src/p2-homeworks/h5/pages/HomeworkFrom9To10.tsx
+++ src/p2-homeworks/h5/pages/HomeworkFrom9To10.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import HW10 from "../../h10/HW10";
 import HW9 from "../../h9/HW9";
 
 export function HomeworkFrom9To10() {
     return (
         <div>
             <HW9 />
+            <HW10 />
         </div>
     );
 }
